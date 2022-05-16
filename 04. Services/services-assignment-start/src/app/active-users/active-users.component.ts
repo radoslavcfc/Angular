6b4ef6bf5a88407b6240cbe111fc3f21272afbe3
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2022-05-16 12:04:47 +0300

    Update active-users.component.ts

--- 04. Services/services-assignment-start/src/app/active-users/active-users.component.ts
+++ 04. Services/services-assignment-start/src/app/active-users/active-users.component.ts
@@ -1,15 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
-  styleUrls: ['./active-users.component.css']
+  styleUrls: ['./active-users.component.css'],
+  providers: [UsersService]
 })
 export class ActiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+  users: string[];
+  constructor(private usersService: UsersService){
+    this.users = this.usersService.activeUsers;
+  }
 
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    this.usersService.onSetToInactive(id);
   }
 }
