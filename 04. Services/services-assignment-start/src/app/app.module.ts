d67b9d322f3ba2314ed3cbd3155ef52f1d204288
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2022-05-18 19:38:30 +0300

    Counting service sorted out

--- 04. Services/services-assignment-start/src/app/app.module.ts
+++ 04. Services/services-assignment-start/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
--- 04. Services/services-assignment-start/src/app/counter.service.ts
+++ 04. Services/services-assignment-start/src/app/counter.service.ts
@@ -2,5 +2,16 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class CounterService {
-    
+    activeToInactiveCount = 0;
+    inactiveToActiveCount = 0;
+
+    increaseActToInact(){
+        this.activeToInactiveCount++;
+        console.log('Active to inactive count: ' + this.activeToInactiveCount)
+    }
+
+    increaseInactToAct(){
+        this.inactiveToActiveCount++;
+        console.log('Inactive to active count: ' + this.inactiveToActiveCount)
+    }
 }
